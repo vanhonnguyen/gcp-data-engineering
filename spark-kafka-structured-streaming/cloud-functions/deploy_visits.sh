e0fcf40268679b222cc093eb0929c8366676d590
NVH <vanhonnguyen@gmail.com>
2021-04-22 22:52:21 +1000

    Delete spark-kafka-structured-streaming directory

--- spark-kafka-structured-streaming/cloud-functions/deploy_visits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FUNCTION="update_visits_by_categories"
-BUCKET="gs://your_bucket_name"
-
-gcloud functions deploy ${FUNCTION} \
-    --runtime python37 \
-    --trigger-resource ${BUCKET} \
-    --trigger-event google.storage.object.finalize
--- spark-kafka-structured-streaming/spark-etl/hive-scripts/submit-hive-etl.sh
+++
@@ -1,2 +0,0 @@
-gcloud dataproc jobs submit pyspark spark-etl/hive-scripts/hivesql-etl.py \
---cluster=hive-sql --region=us-central1 
